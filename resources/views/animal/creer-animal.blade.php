c32eaccee484191e531017d4a96fc2d03c04e998
OussEng <oaitlahssaine@gmail.com>
2026-04-24 16:39:04 +0200

    feat: allow decimal input for weight in animal creation and modification forms

--- resources/views/animal/creer-animal.blade.php
+++ resources/views/animal/creer-animal.blade.php
@@ -55,7 +55,7 @@
 
 
                 <label for="poids" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Poids :</label>
-                <input type="number" id="poids" name="poids"
+                <input type="number" step="0.1" id="poids" name="poids"
                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                        placeholder="Poids... (en Kg)"/>
 
--- resources/views/animal/modifier-animal.blade.php
+++ resources/views/animal/modifier-animal.blade.php
@@ -65,7 +65,7 @@
                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
 
                     <label for="poids" class="block mb-2 mt-4 text-sm font-medium text-gray-900 dark:text-white">Poids :</label>
-                    <input type="number" id="poids" name="poids"
+                    <input type="number" step="0.1" id="poids" name="poids"
                            value="{{ old('poids', $animal->poids) }}"
                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                            placeholder="Poids... (en Kg)" />
